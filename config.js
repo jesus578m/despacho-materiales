9627b12b00ae7449dbaf39b73bad286ae5a8c230
jesus578m <122491133+jesus578m@users.noreply.github.com>
2025-10-14 17:33:31 -0600

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+// config.js
 export default {
   github: {
     owner: "jesus578m",
-    repo: "despacho-materiales",
+    repo:  "despacho-materiales",
     branch: "main",
-    token: null
+    token: "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"
   }
 };
