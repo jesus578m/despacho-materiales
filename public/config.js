26dcd40eb6cceef76d2a812c5f80d020856232d3
jesus578m <122491133+jesus578m@users.noreply.github.com>
2025-10-15 20:33:30 -0700

    Set API base URL in config.js

--- public/config.js
+++ public/config.js
@@ -1,9 +1,1 @@
-// config.js
-export default {
-  github: {
-    owner: "jesus578m",
-    repo:  "despacho-materiales",
-    branch: "main",
-    token: "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"
-  }
-};
+window.API_BASE = '/api';
